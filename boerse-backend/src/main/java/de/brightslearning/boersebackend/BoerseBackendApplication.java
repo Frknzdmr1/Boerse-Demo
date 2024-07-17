b15f12396bc77146f957a27d13ac5c15946a7164
epinark <122348695+epinark@users.noreply.github.com>
2024-07-17 10:39:58 +0200

    initial commit

---
+++ boerse-backend/src/main/java/de/brightslearning/boersebackend/BoerseBackendApplication.java
@@ -0,0 +1,13 @@
+package de.brightslearning.boersebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoerseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoerseBackendApplication.class, args);
+	}
+
+}
---
+++ boerse-backend/src/test/java/de/brightslearning/boersebackend/BoerseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.brightslearning.boersebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoerseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
