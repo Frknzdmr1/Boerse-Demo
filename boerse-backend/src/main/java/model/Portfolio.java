17804800c80a81703c6ec0ea1d44ceeed859eb14
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-07-18 14:57:55 +0200

    Hinzufügen der Portfolio Entity und des DAO

---
+++ boerse-backend/src/main/java/model/Portfolio.java
@@ -0,0 +1,63 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "portfolios")
+public class Portfolio {
+
+    //@OneToMany(mappedBy = "portfolio_aktien")
+//    List<PortfolioAktie> portfolioAktien;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "benutzer_id")
+    private UUID benutzer_id;
+
+    @Column(name = "erstellt_am")
+    private ZonedDateTime erstelltAm;
+
+    @Column(name = "aktualisiert_am")
+    private ZonedDateTime aktualisiertAm;
+
+//    @ManyToOne
+//    @JoinColumn(name = "benutzer")
+//    private Benutzer benutzer;
+
+
+    public Portfolio(UUID id, UUID benutzer_id, ZonedDateTime erstelltAm){
+        this.id = id;
+        this.benutzer_id = benutzer_id;
+        this.erstelltAm = erstelltAm;
+        aktualisiertAm = erstelltAm;
+    }
+
+    public Portfolio(UUID benutzer_id, ZonedDateTime erstelltAm){
+
+        this.id = UUID.randomUUID();
+        this.benutzer_id = benutzer_id;
+        this.erstelltAm = erstelltAm;
+        this.aktualisiertAm = erstelltAm;
+
+    }
+
+    public void setAktualisiertAm(ZonedDateTime aktualisiertAm) {
+        this.aktualisiertAm = aktualisiertAm;
+    }
+}
