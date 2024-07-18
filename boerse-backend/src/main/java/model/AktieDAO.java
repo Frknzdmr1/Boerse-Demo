ed36682ac837bb8783db9fecc4474acc27395d71
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-07-18 14:51:17 +0200

    Hinzufügen von der DAO zu der Entity Aktie.

---
+++ boerse-backend/src/main/java/model/AktieDAO.java
@@ -0,0 +1,10 @@
+package model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AktieDAO extends JpaRepository<Aktie, UUID> {
+}
