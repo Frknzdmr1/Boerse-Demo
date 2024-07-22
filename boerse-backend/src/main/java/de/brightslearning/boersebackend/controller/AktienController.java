8078deb08e9a831591de0ad0db3a1d08112e4922
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-07-22 14:13:52 +0200

    unbenutzte Importe entfernt.

--- boerse-backend/src/main/java/de/brightslearning/boersebackend/controller/AktienController.java
+++ boerse-backend/src/main/java/de/brightslearning/boersebackend/controller/AktienController.java
@@ -3,18 +3,12 @@ package de.brightslearning.boersebackend.controller;
 import de.brightslearning.boersebackend.response_model.PreviousClose;
 import de.brightslearning.boersebackend.service.AktienService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Objects;
 
-//https://api.polygon.io/v2/last/nbbo/AAPL?apiKey=... Apple aktie Last Quote
 
 @RequestMapping("/aktie")
 @RestController
