667dd8935048369b70aa6d00187df87a0fa560cf
Roger Hope <mailroger@rogerhope.de>
2024-07-24 11:50:34 +0200

    "feat: setup of chakraCSS"

--- boerse-frontend/src/App.jsx
+++ boerse-frontend/src/App.jsx
@@ -1,17 +1,43 @@
 import './App.css'
+import HomePage from "@/templates/HomePage";
+import {Route, Routes} from "react-router-dom";
+//import Layout from "@/components/Layout";
+import MyAssetsPage from "@/templates/MyAssetsPage";
+import AssetPage from "@/templates/TokenPage";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-
-
     return (
-        <>
-            <p>Hello World</p>
-            <Navbar />
-            <Seitenhauptteil />
-            <Footer />
+        <ChakraProvider>
+            <Routes>
+                <Route path="/">
+                    <Route
+                        index
+                        element={
 
-        </>
+                            <HomePage />
+
+                        }
+                    />
+                    <Route
+                        path="my-assets"
+                        element={
+                            <div title="My Assets">
+                                <MyAssetsPage />
+                            </div>
+                        }
+                    />
+                    <Route
+                        path="token"
+                        element={
+                            <div title="Tokens">
+                                <AssetPage />
+                            </div>
+                        }
+                    />
+                </Route>
+            </Routes>
+        </ChakraProvider>
     )
 }
-
 export default App
--- boerse-frontend/src/main.jsx
+++ boerse-frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import './App.css';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from '@/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "@/dev";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
+                <App />
+            </DevSupport>
+        </BrowserRouter>
+    </React.StrictMode>,
+);
