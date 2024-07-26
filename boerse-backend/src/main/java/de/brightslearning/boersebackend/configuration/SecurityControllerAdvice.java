f4bb5b88411bc4851e620b52b569efdabd142482
Thorsten Thomann <128320582+Toto149@users.noreply.github.com>
2024-07-26 10:47:25 +0200

    Update SecurityControllerAdvice.java
    Auskommentieren von SecurityControllerAdvice

--- boerse-backend/src/main/java/de/brightslearning/boersebackend/configuration/SecurityControllerAdvice.java
+++ boerse-backend/src/main/java/de/brightslearning/boersebackend/configuration/SecurityControllerAdvice.java
@@ -1,29 +1,42 @@
-package de.brightslearning.boersebackend.configuration;
+//package de.brightslearning.boersebackend.configuration;
+//
+//
+//import de.brightslearning.boersebackend.model.Benutzer;
+//import de.brightslearning.boersebackend.repository.BenutzerRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//
+//import java.util.Collection;
+//import java.util.stream.Collectors;
+//
+//@ControllerAdvice
+//public class SecurityControllerAdvice {
+//
+//    private final BenutzerRepository benutzerRepository;
+//
+//    @Autowired
+//    public SecurityControllerAdvice(BenutzerRepository benutzerRepository) {
+//        this.benutzerRepository = benutzerRepository;
+//    }
+//
+//    @ModelAttribute("sessionUser")
+//    public Benutzer sessionUser(@AuthenticationPrincipal UserDetails userDetails) {
+//        Benutzer benutzer = new Benutzer();
+//        if (userDetails != null) {
+//             benutzer = benutzerRepository.findBenutzername(userDetails.getUsername()).orElseThrow(
+//                    () -> new UsernameNotFoundException("No User with this name found!")
+//            );
+//        }
+//        return null;
+//    }
+//
+//
+//}
 
-
-import de.brightslearning.boersebackend.model.Benutzer;
-import de.brightslearning.boersebackend.repository.BenutzerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-@ControllerAdvice
-public class SecurityControllerAdvice {
-
-    private final BenutzerRepository benutzerRepository;
-
-    @Autowired
-    public SecurityControllerAdvice(BenutzerRepository benutzerRepository) {
-        this.benutzerRepository = benutzerRepository;
-    }
-
-    @ModelAttribute("sessionUser")
-    public Benutzer sessionUser(@AuthenticationPrincipal UserDetails userDetails) {
-        if (userDetails != null) {
-            return benutzerRepository.findBenutzerByBenutzername(userDetails.getUsername());
-        }
-        return null;
-    }
-}
