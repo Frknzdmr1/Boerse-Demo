bbb71408bd5e76e4541a7e41b2de3cb1aa93ebf1
Thorsten Thomann <128320582+Toto149@users.noreply.github.com>
2024-07-29 22:01:58 +0200

    Update PortfolioDTO.java
    Füge id als Attribut zum record PortfolioDto hinzu.

--- boerse-backend/src/main/java/de/brightslearning/boersebackend/Dto/PortfolioDTO.java
+++ boerse-backend/src/main/java/de/brightslearning/boersebackend/Dto/PortfolioDTO.java
@@ -3,7 +3,8 @@ package de.brightslearning.boersebackend.Dto;
 import de.brightslearning.boersebackend.model.PortfolioAktie;
 
 import java.util.List;
+import java.util.UUID;
 
-public record PortfolioDTO(List<AktieDTO> portfolioAktien)
-
-{}
+public record PortfolioDTO(UUID id, List<AktieDTO> portfolioAktien){
+  
+}
