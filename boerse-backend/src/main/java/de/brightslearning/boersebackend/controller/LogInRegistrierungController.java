39f6398b0f3079af3841a4362b23a0c4eab8ce14
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-07-30 11:15:46 +0200

    Hinzufügen von JWTGenerator zu dem LogInRegistrierungController und hinzufügen von einigen Importen dafür.

--- boerse-backend/src/main/java/de/brightslearning/boersebackend/controller/LogInRegistrierungController.java
+++ boerse-backend/src/main/java/de/brightslearning/boersebackend/controller/LogInRegistrierungController.java
@@ -1,6 +1,9 @@
 package de.brightslearning.boersebackend.controller;
 
+import de.brightslearning.boersebackend.Dto.AuthResponseDto;
+import de.brightslearning.boersebackend.Dto.LoginDto;
 import de.brightslearning.boersebackend.Dto.RegisterDto;
+import de.brightslearning.boersebackend.configuration.JWTGenerator;
 import de.brightslearning.boersebackend.model.Benutzer;
 import de.brightslearning.boersebackend.model.UserRolle;
 import de.brightslearning.boersebackend.repository.BenutzerRepository;
@@ -9,9 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
@@ -19,6 +26,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/auth")
 public class LogInRegistrierungController {
 
 
@@ -26,14 +34,38 @@ public class LogInRegistrierungController {
     private BenutzerRepository benutzerRepository;
     private UserRolleRepository userRolleRepository;
     private PasswordEncoder passwordEncoder;
+    private JWTGenerator jwtGenerator;
+
 
     @Autowired
     public LogInRegistrierungController(AuthenticationManager authenticationManager, BenutzerRepository benutzerRepository,
-                                        UserRolleRepository userRolleRepository, PasswordEncoder passwordEncoder) {
+                                        UserRolleRepository userRolleRepository, PasswordEncoder passwordEncoder,
+                                        JWTGenerator jwtGenerator) {
         this.authenticationManager = authenticationManager;
         this.benutzerRepository = benutzerRepository;
         this.userRolleRepository = userRolleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.benutzername(),
+                        loginDto.password())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerator.generateToken(authentication);
+
+
+
+//        Benutzer benutzer = benutzerRepository.findByBenutzername(loginDto.benutername()).get();
+//        if(benutzer == null || !passwordEncoder.matches(loginDto.password(), benutzer.getPasswort())){
+//            return new ResponseEntity<>("Der Benutzername oder das Passwort sind falsch!", HttpStatus.BAD_REQUEST);
+//        }
+
+        return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
     }
 
     @PostMapping("/register")
@@ -54,8 +86,6 @@ public class LogInRegistrierungController {
         );
 
         benutzerRepository.save(benutzer);
-
-
         return new ResponseEntity<>("Benutzer wurde erfolgreich registriert", HttpStatus.OK);
     }
 }
