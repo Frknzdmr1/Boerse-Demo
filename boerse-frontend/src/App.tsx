d5cddda82c55aed9300cdc5420ad1dc26fa17d41
Roger Hope <mailroger@rogerhope.de>
2024-07-30 16:31:39 +0200

    "feat: Seite hinzugefügt: Eine neue Seite mit einer Liste aller Preise"

--- boerse-frontend/src/App.tsx
+++ boerse-frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Aktien from '@/components/Aktien';
 import Portfolio from '@/components/Portfolio';
 import AktuellesPage from "@/pages/AktuellesPage";
 import TokenPage from "@/pages/TokenPage";
+import HandelnPage from "@/pages/HandelnPage";
 
 function App() {
     return (
@@ -28,6 +29,7 @@ function App() {
                 <Route path="/aktuelles" element={<AktuellesPage />} />
                 <Route path="/current-price" element={<Aktien/>}/>
                 <Route path="/token/:symbol" index element={<TokenPage/>}/>
+                <Route path="/handeln" index element={<HandelnPage />}/>
             </Routes>
         </ChakraProvider>
     );
