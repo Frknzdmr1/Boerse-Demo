f6f5935d236d03768c22d18ee9e09ee49838a7ec
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-07-30 11:16:39 +0200

    Hinzufügen einer DatenKlasse AuthResponseDto.java zum empfangen der AuthResponse Daten.

---
+++ boerse-backend/src/main/java/de/brightslearning/boersebackend/Dto/AuthResponseDto.java
@@ -0,0 +1,13 @@
+package de.brightslearning.boersebackend.Dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDto{
+    private String accessToken;
+    private String tokenType = "Bearer ";
+
+    public AuthResponseDto(String accessToken){
+        this.accessToken = accessToken;
+    }
+}
