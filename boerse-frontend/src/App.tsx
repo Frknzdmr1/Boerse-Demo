4bc14e4186dc135d5909efd50965357e1d7fdc81
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-07-30 11:10:32 +0200

    Hinzufügen von Login-Element zur App.tsx

--- boerse-frontend/src/App.tsx
+++ boerse-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import HomePage from '@/pages/HomePage';
@@ -6,28 +6,20 @@ import Aktien from '@/components/Aktien';
 import Portfolio from '@/components/Portfolio';
 import AktuellesPage from "@/pages/AktuellesPage";
 import TokenPage from "@/pages/TokenPage";
+import Login from "@/pages/Login/Login";
 
 function App() {
+    const [userId, setUserId] = useState<string>("");
     return (
         <ChakraProvider>
             <Routes>
-                <Route path="/">
-                    <Route
-                        index
-                        element={
-
-                            <HomePage />
-
-                        }
-
-                    />
-
-                </Route>
+                <Route path="/" element={<HomePage />} />
                 <Route path="/current-price" element={<Aktien />} />
-                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/portfolio" element={<Portfolio userId={userId} />} />
                 <Route path="/aktuelles" element={<AktuellesPage />} />
                 <Route path="/current-price" element={<Aktien/>}/>
                 <Route path="/token/:symbol" index element={<TokenPage/>}/>
+                <Route path="/login" element={<Login setUserId={setUserId}/>} />
             </Routes>
         </ChakraProvider>
     );
