484dc93f9285c7adac900f4fb56a29143dd02a73
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-07-31 01:49:37 +0200

    Ändere Klasse so um, dass der authHeader genommen wird um den Bearer Token auszulesen.

--- boerse-backend/src/main/java/de/brightslearning/boersebackend/configuration/JWTAuthenticationFilter.java
+++ boerse-backend/src/main/java/de/brightslearning/boersebackend/configuration/JWTAuthenticationFilter.java
@@ -6,15 +6,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.util.StringUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 @NoArgsConstructor
+@Component
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     private JWTGenerator tokenGenerator;
@@ -29,33 +31,33 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                 HttpServletResponse response,
-                                 FilterChain filterChain) throws ServletException, IOException {
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        String token = getJWTFromRequest(request);
+        String authHeader = request.getHeader("Authorization");
 
-        if(StringUtils.hasText(token) && tokenGenerator.validateToken(token)){
-            String benutzername = tokenGenerator.getBenutzernameVonJWT(token);
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
 
-            UserDetails userDetails = securityService.loadUserByUsername(benutzername);
+        }
+        String token = authHeader.substring(7);
+        String benutzername = tokenGenerator.getBenutzernameVonJWT(token);
+        UserDetails userDetails = securityService.loadUserByUsername(benutzername);
+        if(userDetails != null
+                && SecurityContextHolder.getContext().getAuthentication() == null
+                && tokenGenerator.validateToken(token, userDetails)) {
 
-            if(userDetails != null) {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,
                         userDetails.getAuthorities());
-
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
 
-        }
-        filterChain.doFilter(request, response);
-    }
 
-    private String getJWTFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7); // Der Token startet nach 'Bearer ', was beim Indize 7 ist.
+        filterChain.doFilter(request, response);
         }
-        return null;
-    }
+
+
+
 }
