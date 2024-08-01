9a9cd8c2267997637191bfcad72c2e759a9b7592
Roger Hope <mailroger@rogerhope.de>
2024-08-01 09:43:53 +0200

    "refactor: Aktien component added"

--- boerse-frontend/src/pages/HomePage/index.tsx
+++ boerse-frontend/src/pages/HomePage/index.tsx
@@ -5,6 +5,7 @@ import Layout from "@/components/Layout";
 import TopTokens from "./TopMarken";
 import NeuesteAktionen from "./NeuesteAktionen";
 import Learnings from "@/pages/HomePage/Learnings";
+import Aktien from "@/components/Aktien";
 
 const HomePage = () => {
 
@@ -12,6 +13,7 @@ const HomePage = () => {
     return (
         <Layout title="Dashboard">
             <div className="space-y-2">
+                <Aktien />
 
 
                 <div className="flex space-x-2 lg:block lg:space-x-0 lg:space-y-2">
